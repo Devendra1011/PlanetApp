1b73744797530050b044116594e5b775f1db7f74
Devendra1011 <devendrabhatta63@gmail.com>
2024-09-23 22:48:08 +0545

    final update

--- app/src/main/java/com/example/planetapp/MainActivity.kt
+++ app/src/main/java/com/example/planetapp/MainActivity.kt
@@ -9,7 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var listView:ListView
+
+    // 1- view
+    lateinit var listView: ListView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +26,32 @@ class MainActivity : AppCompatActivity() {
 
         listView = findViewById(R.id.list_view)
 
+
+        var planetsList = ArrayList<Planet>()
+
+        val planet1 = Planet("Mercury", "0 Moon", R.drawable.mercury)
+        val planet2 = Planet("Venus", "0 Moon", R.drawable.venus)
+        val planet3 = Planet("Earth", "1 Moon", R.drawable.earth)
+        val planet4 = Planet("Mars", "2 Moon", R.drawable.mars)
+        val planet5 = Planet("Jupiter", "79 Moon", R.drawable.jupiter)
+        val planet6 = Planet("Saturn", "83 Moon", R.drawable.saturn)
+        val planet7 = Planet("Uranus", "27 Moon", R.drawable.uranus)
+        val planet8 = Planet("Neptune", "14 Moon", R.drawable.neptune)
+        val planet9 = Planet("Pluto", "1 M|oon", R.drawable.pluto)
+
+        planetsList.add(planet1)
+        planetsList.add(planet2)
+        planetsList.add(planet3)
+        planetsList.add(planet4)
+        planetsList.add(planet5)
+        planetsList.add(planet6)
+        planetsList.add(planet7)
+        planetsList.add(planet8)
+        planetsList.add(planet9)
+
+
+        // adapter
+        var adapter = PlanetAdapter(this,planetsList)
+        listView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/planetapp/PlanetAdapter.kt
+++ app/src/main/java/com/example/planetapp/PlanetAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class PlanetAdapter(val context: Context, val planets: List<Planet>) : BaseAdapter() {
     override fun getCount(): Int {
@@ -26,8 +29,31 @@ class PlanetAdapter(val context: Context, val planets: List<Planet>) : BaseAdapt
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
         // Return a View that displays the data at the specified position in the list
-        val inflater  = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.list_item_layout, parent, false)
 
+        } else {
+            // Reuse the convertView
+            view = convertView
+        }
+        // Bind data to the view
+        val item = getItem(position) as Planet
+        // Initialize the views
+        val planetName = view.findViewById<TextView>(R.id.lil_tv_planetName)
+        val moonCount = view.findViewById<TextView>(R.id.lil_tv_moonCount)
+        val planetImage = view.findViewById<ImageView>(R.id.lil_iv_planet)
+
+        planetImage.setImageResource(item.planetImage)
+        moonCount.text = item.moonCount
+        planetName.text = item.planet
+
+        // Handling CLick Events
+        view.setOnClickListener{
+            Toast.makeText(context,"You CLicked: ${planets[position].planet}",Toast.LENGTH_SHORT).show()
+
+        }
+        return view
     }
-
 }
